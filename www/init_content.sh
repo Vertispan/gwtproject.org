435aa71bd63d60c64c985e92a3b24f43c7952db5
Colin Alworth <colin@vertispan.com>
2024-12-19 16:53:28 -0600

    Rebuild www content to unpack CI artifacts (#9)
    This will delay initial startup slightly, but will in general be a
    faster and simpler build than we previously had. Webhooks are
    disabled right now, but when ready we can initiate deployments
    automatically when new builds are ready.

---
+++ www/init_content.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# Simple script to update and replace www content. Future revisions should pull javadoc
+# separately (from the latest release), and possible separate each of the three different
+# content sources to let only one update at a time more quickly.
+
+# Show us commands, stop on errors, and fail on missing vars
+set -eux
+
+# Create a temp dir to do our work in
+WORK_DIR=$(mktemp -d)
+
+# Download the latest builds for gwt-site and gwt-site-webapp
+curl -L -o "$WORK_DIR/gwt-site.zip" https://nightly.link/gwtproject/gwt-site/workflows/build/main/gwt-site.zip
+curl -L -o "$WORK_DIR/gwt-site-webapp.zip" https://nightly.link/gwtproject/gwt-site-webapp/workflows/build/main/gwt-site-webapp.zip
+
+# Unzip the files
+mkdir "$WORK_DIR/www"
+unzip "$WORK_DIR/gwt-site.zip" -d "$WORK_DIR/www"
+unzip "$WORK_DIR/gwt-site-webapp.zip" -d "$WORK_DIR/www"
+
+# Add/delete/update files as needed from the hosted directory
+rsync -avh --delete "$WORK_DIR/www/" /usr/share/nginx/html/
+
+# Clean up work directory
+rm -r "$WORK_DIR"
